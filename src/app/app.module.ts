0a37b2cd31bcbfb950ab27109f71ef9ee239ee62
Coralie2798 <coralie.bodiguel@gmail.com>
2023-03-31 12:07:08 +0200

    0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ConnexionFormulaireComponent } from './component/connexion-formulaire/c
     UtilisateurComponent,
     FormulaireInscriptionComponent,
     ConnexionFormulaireComponent
+
   ],
   imports: [
     BrowserModule,
