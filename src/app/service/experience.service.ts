ef903ff2b1e5626ec47abc7fa3f1738519b85ad9
=zwileo <=leo.zwilling@orange.fr>
2023-04-03 13:42:56 +0200

    iugboiuyg

--- src/app/service/experience.service.ts
+++ src/app/service/experience.service.ts
@@ -1,13 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-import { Injectable } from '@angular/core';
-import { Trajet } from '../model/trajet.model';
-=======
 
 import { Injectable } from '@angular/core';
 import { Experience } from '../model/experience.model';
 import { Restaurant } from '../model/restaurant';
->>>>>>> 6b6ad63520f1787a27e958f1347e98a87b3adaac
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -15,28 +10,6 @@ import { Observable } from 'rxjs';
 })
 export class ExperienceService {
 
-<<<<<<< HEAD
- 
-  constructor(private http:HttpClient) { }
-
-  getTrajet():Observable<Trajet[]>
-  {  
-    
-    return this.http.get<Trajet[]>("http://localhost:8080/trajet/listeTrajet");
-  }
-
-  addTrajet(t:Trajet):Observable<Trajet>
-  {
-    return this.http.post<Trajet>("http://localhost:8080/trajet/saveTrajet", t);
-  }
-
-
-
-   delete(id_T:number):Observable<void>
-   {
-     return this.http.delete<void>("http://localhost:8080/trajet/deleteTrajet/" + id_T)
-   }
-=======
   constructor(private http:HttpClient){}
 
   addRestaurant(resto:Restaurant):Observable<Restaurant>
@@ -55,5 +28,4 @@ export class ExperienceService {
     return this.http.get<Experience>("http://localhost:8080/utilisateur/getExperience", e)
   }
 
->>>>>>> 6b6ad63520f1787a27e958f1347e98a87b3adaac
 }
