31c103b83da06a40749dae8c0c8c437d8ab8f54e
=zwileo <=leo.zwilling@orange.fr>
2023-04-03 12:34:54 +0200

    erreurs resolues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { ExperienceComponent } from './component/experience/experience.component
 import { ConnexionFormulaireComponent } from './component/connexion-formulaire/connexion-formulaire.component';
 import { FormulaireInscriptionComponent } from './component/formulaire-inscription/formulaire-inscription.component';
 import { UtilisateurComponent } from './component/utilisateur/utilisateur.component';
-import { LieuxComponent } from './lieux/lieux.component';
-import { FormulaireLieuxComponent } from './formulaire-lieux/formulaire-lieux.component';
 import { FormulaireExperienceGeneraleComponent } from './formulaire-experience-generale/formulaire-experience-generale.component';
 
 
@@ -14,9 +12,6 @@ const routes: Routes = [
   {path:"experience", component:ExperienceComponent},
   {path:"utilisateur", component:UtilisateurComponent},
   {path:"inscription", component:FormulaireInscriptionComponent},
-  {path:"connexion", component:ConnexionFormulaireComponent},
-  {path:"lieux", component:LieuxComponent},
-  {path:"lieuxformulaire", component:FormulaireLieuxComponent},
   {path:"ajouterExperience", component:FormulaireExperienceGeneraleComponent}
 ];
 
