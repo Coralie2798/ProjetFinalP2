144ef2b0f0ed0b190bc64705dbfdb175d7abb4c2
Coralie2798 <coralie.bodiguel@gmail.com>
2023-04-03 09:50:45 +0200

    ajout component trajet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { ExperienceComponent } from './component/experience/experience.component';
-
-
-=======
 import { UtilisateurComponent } from './component/utilisateur/utilisateur.component';
 import { FormulaireInscriptionComponent } from './component/formulaire-inscription/formulaire-inscription.component';
 import { FormGroup,ReactiveFormsModule, FormsModule} from '@angular/forms';
 import { ConnexionFormulaireComponent } from './component/connexion-formulaire/connexion-formulaire.component';
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> 698a33edcca081253b8741cdbb345fea730cd7e9
+import { Experience } from './model/experience.model';
+import { ExperienceComponent } from './component/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ExperienceComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-=======
     UtilisateurComponent,
     FormulaireInscriptionComponent,
-    ConnexionFormulaireComponent 
+    ConnexionFormulaireComponent,
+    ExperienceComponent
 
   ],
   imports: [
@@ -36,7 +26,6 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule
->>>>>>> 698a33edcca081253b8741cdbb345fea730cd7e9
     
   ],
   providers: [],
