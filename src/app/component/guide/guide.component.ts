bf0a7c6fafe74efe130e808a6d9bdda93c4b2008
Thaou15 <thaoura.mhoma@laposte.net>
2023-04-05 16:09:12 +0200

    select

--- src/app/component/guide/guide.component.ts
+++ src/app/component/guide/guide.component.ts
@@ -2,7 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Guide } from 'src/app/model/guide.model';
+import { Ville } from 'src/app/model/ville.model';
 import { GuideService } from 'src/app/service/guide.service';
+import { VilleService } from 'src/app/service/ville.service';
 
 @Component({
   selector: 'app-guide',
@@ -12,16 +14,30 @@ import { GuideService } from 'src/app/service/guide.service';
 export class GuideComponent implements OnInit{
   
   listeG$!: Observable<Guide[]>;
-  constructor(private gs:GuideService, private router:Router){}
+  listeV$!:Observable<Ville[]>;
+  listeGV$!:Observable<Guide[]>;
+  v!:Ville;
+  villechoisie:boolean=false;
+  constructor(private gs:GuideService,private vs:VilleService, private router:Router){}
 
   ngOnInit(): void {
     this.listeG$ = this.gs.getGuide();
+    this.listeV$ = this.vs.getVille();
+   
   }
 
   supprimer(id_guide:number)
   {
     this.gs.delete(id_guide).subscribe(()=>{this.ngOnInit()});
     this.router.navigate(['guide']);
+  }
+
+  villeselectionne(v:Ville){
+
+    this.v = v;
+    this.listeG$= this.gs.getGuideIDV(v.id_ville);
+    this.villechoisie=true;
+
   }
 
 
