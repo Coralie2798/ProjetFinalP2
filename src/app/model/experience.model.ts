cfa7234c086fcbbd0c6837c77bc93e8166357879
=zwileo <=leo.zwilling@orange.fr>
2023-04-11 10:37:59 +0200

    reparation ajouter experience

--- src/app/model/experience.model.ts
+++ src/app/model/experience.model.ts
@@ -5,14 +5,18 @@ export class Experience {
     photo:string;
     depense:number;
     rating_moyen:number;
+    id_trajet:number;
+    id_ville:number;
 
-    constructor( description:string, photo:string, depense:number, rating_moyen:number, id_voyage:number)
+    constructor( description:string, photo:string, depense:number, rating_moyen:number, id_voyage:number, id_trajet:number, id_ville:number)
     {
         this.id_voyage=id_voyage
         this.description=description,
         this.photo=photo,
         this.depense=depense,
-        this.rating_moyen=rating_moyen
+        this.rating_moyen=rating_moyen,
+        this.id_trajet=id_trajet;
+        this.id_ville=id_ville;
         
     }
 
