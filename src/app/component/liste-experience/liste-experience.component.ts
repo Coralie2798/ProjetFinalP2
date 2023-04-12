a5f4753f77648a5a5525d6e743bbc99d5399eb8b
InfoJavaF <florian.belin@itech.fr>
2023-04-12 08:38:46 +0200

    experience

--- src/app/component/liste-experience/liste-experience.component.ts
+++ src/app/component/liste-experience/liste-experience.component.ts
@@ -4,8 +4,10 @@ import { Observable } from 'rxjs';
 import { Experience } from 'src/app/model/experience.model';
 import { Lieux } from 'src/app/model/lieux.model';
 import { Restaurant } from 'src/app/model/restaurant';
+import { Ville } from 'src/app/model/ville.model';
 import { ExperienceService } from 'src/app/service/experience.service';
 import { LieuxService } from 'src/app/service/lieux.service';
+import { VilleService } from 'src/app/service/ville.service';
 
 @Component({
   selector: 'app-liste-experience',
@@ -14,15 +16,15 @@ import { LieuxService } from 'src/app/service/lieux.service';
 })
 export class ListeExperienceComponent implements OnInit {
 
-  constructor(private es:ExperienceService, private router:Router, private ls:LieuxService, private ar:ActivatedRoute){this.id=ar.snapshot.params["id"]}
+  constructor(private vs:VilleService ,private es:ExperienceService, private router:Router, private ls:LieuxService, private ar:ActivatedRoute){this.id=ar.snapshot.params["id"]}
   
   exp$!:Observable<Experience>;
   id!:number;
   
   listeE$!: Observable<Experience[]>;
   listeR$!: Observable<Restaurant[]>;
-  listeL$!:Observable<Lieux[]>;
-
+  listeL$!: Observable<Lieux[]>;
+  destination!: Observable<Ville>
  
 
   
@@ -36,13 +38,17 @@ export class ListeExperienceComponent implements OnInit {
    this.listeR$=this.es.getRestaurant();
    this.listeL$=this.ls.getLieux();
    
+   
   }
 
 
 afficherExperience(id:number) {
   console.log(id);
+  
   this.exp$=this.es.getExperienceById(id);
   this.es.idExp=id;
+  this.destination=this.es.getVilleByExp(id);
+  console.log("destination" + this.destination);
   this.router.navigate(['experience/']);
   //this.router.navigate(['experience/' + id]);
 }
